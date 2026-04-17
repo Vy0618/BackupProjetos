a39000bc4e906d607e3dddc8c09ea12e4f8c1e9c
Vy0618 <viniciusazevedo1a@gmail.com>
2026-04-17 16:21:39 -0300

    Update testeMotoresEsc.ino

--- programacaoEcoFlutuador/testeMotoresEsc.ino
+++ programacaoEcoFlutuador/testeMotoresEsc.ino
@@ -1,29 +1,11 @@
- #include <Arduino.h>
+#include <Arduino.h>
 #include <ESP32Servo.h>
 
-const int ESC_PIN1 = 12;
-const int ESC_PIN2 = 14;
+#define ESC_PIN1 12
+#define ESC_PIN2 14
 
 Servo esc1;
 Servo esc2;
-
-
-int pctParaAngulo(int pct) {
-  pct = constrain(pct, 0, 100);
-  return map(pct, 0, 100, 0, 180);
-}
-
-void parar(){
-    esc1.write(0);
-    esc2.write(0);
-   
-}
-
-void avancar(){
-    int velocidade = pctParaAngulo(40);
-    esc1.write(velocidade);
-    esc2.write(velocidade);
-}
 
 void setup() {
   Serial.begin(115200);
@@ -31,16 +13,31 @@ void setup() {
   esc1.attach(ESC_PIN1, 1000, 2000);
   esc2.attach(ESC_PIN2, 1000, 2000);
 
-  parar();
+  esc1.writeMicroseconds(1000);
+  esc2.writeMicroseconds(1000);
   delay(3000);
- 
 }
 
-void loop() {
-    
-    avancar();
-    delay(5000);
-    parar();
-    delay(5000);
-    
+int pctParaMicro(int pct) {
+  pct = constrain(pct, 0, 100);
+  return map(pct, 0, 100, 1000, 2000);
 }
+
+void parar(){
+  esc1.writeMicroseconds(1000);
+  esc2.writeMicroseconds(1000);
+}
+
+void avancar(){
+  int velocidade = pctParaMicro(40);
+  esc1.writeMicroseconds(velocidade);
+  esc2.writeMicroseconds(velocidade);
+}
+
+void loop() {    
+  avancar();
+  delay(5000);
+  parar();
+  delay(5000);
+}
+
